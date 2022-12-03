0923c35d3e6aa619bc0ea6d9c3a63fe56ef945b8
Conor Joyce <conor.joyce27@mail.dcu.ie>
2022-12-03 07:16:37 +0000

    add daily setup script

---
+++ setup-day-n.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# mkdir
+n=`printf %02d $1`
+mkdir day-$n
+
+# init main.py
+echo "#!/usr/bin/env python\n" > day-$n/main.py
+
+# get cookie and download input
+export $(xargs < .env)
+curl "https://adventofcode.com/2022/day/$1/input" -H "cookie: $cookie" -o day-$n/input
